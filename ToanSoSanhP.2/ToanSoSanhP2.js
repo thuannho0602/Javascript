d77cf04b8a5b2579b9c348551a528c54d2b58dd8
thuannguyen <thuannho0602@gmail.com>
2023-11-01 23:25:39 +0700

    su khac nhau giửa == và=== end !=và !==

---
+++ ToanSoSanhP.2/ToanSoSanhP2.js
@@ -0,0 +1,24 @@
+/**
+ * Toán tử so sánh
+ *  === 
+ * !==
+ */
+
+/**
+ * sự khác nhau giữa == và ===
+ * toán tử == thì nó chỉ so sanh kiểu value thôi
+ * còn === thì  thì nó so sánh cả hai , Value và kiểu dữ liệu ,nó sẽ chặt chẻ hơn ==
+ */
+var a=2;
+var b='2';
+console.log(a===b)
+
+/**
+ * sự khác nhau giửa != và !==
+ * toán tử != thì nó chỉ quan tâm đến so sanh kiểu value thôi
+ * còn !== thì thì nó so sánh cả hai , Value và kiểu dữ liệu ,nó sẽ chặt chẻ hơn !=
+ */
+
+ var a=2;
+ var b='2';
+ console.log(a!==b)
