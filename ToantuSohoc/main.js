f7d2b6429f1f0f4c994f62ae72e2237e16679de6
thuannguyen <thuannho0602@gmail.com>
2023-11-01 13:40:35 +0700

    toantusohoc

---
+++ ToantuSohoc/main.js
@@ -0,0 +1,44 @@
+// toán tử Số học
+
+
+// Toán tử Cộng
+var a=1;
+var b=2;
+var c=a+b;
+console.log(c);
+// Toán tử trừ
+var a=1;
+var b=2;
+var c=a-b;
+console.log(c);
+
+//tóa tử nhân
+var a=1;
+var b=2;
+var c=a*b;
+console.log(c);
+
+//Toán tử chia (/)
+var a=1;
+var b=2;
+var c=a/b;
+console.log(c);
+
+// Toán tử chia lấy dư
+var a=8;
+var b=4;
+var c=a%b;
+console.log(c);
+
+// Toán tử ++
+var d=7;
+d++;
+d++;
+console.log(d);
+
+
+// Toán tử --
+var k=10;
+k--;
+k++;
+console.log(k);
