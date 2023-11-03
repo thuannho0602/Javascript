01b2b6d651dd7dec0c0e03af80174e14ae3296a8
thuannguyen <thuannho0602@gmail.com>
2023-11-03 09:50:34 +0700

    Return trong JS

---
+++ ReturnTRongJS/return.js
@@ -0,0 +1,10 @@
+/**
+ * Return trong js là gì 
+ * nó là kiểu trả về cho một cái gì đó
+ */
+
+function cong(a,b){
+    return a.toString()+b.toString()
+}
+var result=cong(6,8);
+console.log(result);
