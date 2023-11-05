505cb60b1986595a819a82eee2457b5425c032de
thuannguyen <thuannho0602@gmail.com>
2023-11-05 21:59:05 +0700

    Giới thiệu vòng lặp

---
+++ VongLap/GioiThieuVL.js
@@ -0,0 +1,9 @@
+/** 
+ * Vòng lặp là gì 
+  1.  for- vòng lặp với điều kiện đúng
+  2. for/in - vòng lặp quay key của đối tượng
+  4. whliwe - vòng lặp qua value của đối tượng
+  5. do/while -vòng lặp ít nhất 1 lần , sau đó lặp khi điều kiện đúng
+
+ */
+var a=1000
