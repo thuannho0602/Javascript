0996a347c9e973686297be1671f041ef8bf0d898
thuannguyen <thuannho0602@gmail.com>
2023-11-05 19:20:54 +0700

    Toán tử Switch

---
+++ Toan3Ngoi_IfElse_Swich/Swich.js
@@ -0,0 +1,33 @@
+/**
+ * Switch là gì 
+ * switch case cũng thuộc loại câu lệnh rẽ nhánh như câu lệnh if else nhưng nó sẽ thực hiện các hành động dựa trên một loạt các điều kiện khác nhau.
+ * 
+ */
+
+///  date 6 
+var date=4;
+switch(date){
+    case 1:
+        console.log('Hôm nay Chủ Nhật');
+        break;
+    case 2:
+        console.log('Hôm nay Thứ 2');
+        break;
+    case 3:
+        console.log('Hôm nay Thứ 3');
+        break;
+    case 4:
+        console.log('Hôm nay Thứ 4');
+        break; 
+    case 5:
+        console.log('Hôm nay Thứ 5');
+        break; 
+    case 6:
+        console.log('Hôm nay Thứ 6');
+        break;
+    case 7:
+        console.log('Hôm nay Thứ 7');
+        break;
+    default:
+        console.log('K có trong tuần')
+}
