9ffdf32bbbe3fa39cb5489c57cf70c5b1b607398
thuannguyen <thuannho0602@gmail.com>
2023-11-06 15:56:49 +0700

    For_In JavaScrpit

--- VongLap/For_In.js
+++ VongLap/For_In.js
@@ -4,3 +4,31 @@
      Câu lệnh lặp này có thể được sử dụng với String, Array, or những object đơn giản, nhưng không phải với các object như Map() hoặc Set().
 
  */
+
+     // Đối Tượng 
+var myOject={
+    name:'Thuan',
+    age:23,
+    Address:'Tân Biên Tây Ninh'
+}
+
+for(var key in myOject){
+    console.log(myOject[key]);
+}
+// Mảng 
+var myArry=[
+    'Tân Biên',
+    'Hòa Thành',
+    'Tân Châu',
+    'Châu Thành'
+]
+for(var value in myArry){
+    console.log(myArry[value])
+}
+
+
+// chuỗi 
+ var myChuoi='Nguyễn Minh Thuận';
+for(var chuoi in myChuoi){
+    console.log(myChuoi[chuoi]);
+}
