a5edcf778ad8c393691cbc679fc150a593d3729d
thuannguyen <thuannho0602@gmail.com>
2023-11-06 22:47:22 +0700

    +5 len

--- VongLap/VongLapLongNhau.js
+++ VongLap/VongLapLongNhau.js
@@ -5,15 +5,20 @@
         - Ta thấy vòng lặp bên trong sẽ phải chạy mỗi khi vòng lặp bên ngoài duyệt qua một phần tử.
  */
 // tạo 1 cái mảng và in ra màng hình 123456
-var mayarry=[
-    [1,2],
-    [3,4],
-    [5,6]
-];
-// for cha
-for(var i=0;i < mayarry.length;i++){
-    // for con 
-    for(var j=0; j<mayarry.length;j++){
-        console.log(mayarry[i][j]);
-    }
+// var mayarry=[
+//     [1,2],
+//     [3,4],
+//     [5,6]
+// ];
+// // for cha
+// for(var i=0;i < mayarry.length;i++){
+//     // for con 
+//     for(var j=0; j<mayarry.length;j++){
+//         console.log(mayarry[i][j]);
+//     }
+// }
+//lầy i luon +5
+for(var i=0;i<100;i+=5){
+    console.log(i)
 }
+
