e3b69c9b6eecbac59600c0d679d168d3e704761c
thuannguyen <thuannho0602@gmail.com>
2023-11-06 15:04:11 +0700

    ForIn

---
+++ VongLap/For_In.js
@@ -0,0 +1,6 @@
+/**
+    For/In trong JsLa12 Gì: 
+    được sử dụng để được sử dụng để lặp lại trên tất cả các enumerable properties của một Object, bao gồm enumerable properties được kế thừa.
+     Câu lệnh lặp này có thể được sử dụng với String, Array, or những object đơn giản, nhưng không phải với các object như Map() hoặc Set().
+
+ */
