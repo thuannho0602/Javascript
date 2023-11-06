b9df56ec7e1057ee5c7d01218cd9806b596a9ad6
thuannguyen <thuannho0602@gmail.com>
2023-11-06 21:51:32 +0700

    Vòng lặp While

---
+++ VongLap/While.js
@@ -0,0 +1,17 @@
+/**
+ *  vòng lặp While là gì
+ * vòng lặp while lặp lại một khối mã miễn là điều kiện được chỉ định đánh giá là đúng.
+ */
+// mảng 
+var myArry1=[
+    'JS',
+    'C#',
+    'C++',
+    'C'
+]
+
+var i=0;
+while(i<myArry1.length){
+    i++;
+    console.log(myArry1[i]);
+}
