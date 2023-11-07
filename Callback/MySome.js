ed699083b34c90d9859e062f2728480ef401e986
thuannguyen <thuannho0602@gmail.com>
2023-11-07 15:42:36 +0700

    Some

---
+++ Callback/MySome.js
@@ -0,0 +1,43 @@
+/**
+  My Some là 
+ */
+Array.prototype.some2=function(callback){
+    var outPut=false
+    for(var index in this){
+        if(this.hasOwnProperty(index)){
+            if(callback[index],index,this){
+                return true;
+            }
+        }
+    }
+    return outPut;
+}
+
+  // Tạo list arr
+  var myArrCouns=[
+    {
+        name:'PHP',
+        coins:500,
+        isFinish:false
+    },
+    {
+        name:'C#',
+        coins:1500,
+        isFinish:false
+    },
+    {
+        name:'Java',
+        coins:1500,
+        isFinish:false
+    },
+    {
+        name:'Golang',
+        coins:1000,
+        isFinish:false
+    },
+];
+// xl nó
+var reslut=myArrCouns.some2(function(myArrCoun,index,arr){
+    return myArrCoun.isFinish=true ;
+});
+console.log(reslut);
