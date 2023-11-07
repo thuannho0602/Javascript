d8b235282f4384c75c63e64280f74c4331228e63
thuannguyen <thuannho0602@gmail.com>
2023-11-07 11:22:39 +0700

    Includes Chuỗi Và Arr

---
+++ MangII/includes.js
@@ -0,0 +1,13 @@
+/**
+ * includes() method trong js 
+ * cách sữ dụng
+ * 
+ */
+//SD vs String
+var tileName='Nguyễn minh thuận';
+console.log(tileName.includes('n'))
+
+
+// sử dụng với Arr
+var ArrNamr=['CSS','HTML','Javascript','PHP']
+console.log(ArrNamr.includes('PHP'))
