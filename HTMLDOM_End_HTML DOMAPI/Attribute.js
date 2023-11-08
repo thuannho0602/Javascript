2c59c4a6c2e0bc911bb65e0b085ae86843f2ad98
thuannguyen <thuannho0602@gmail.com>
2023-11-08 11:18:25 +0700

    html dom

---
+++ HTMLDOM_End_HTML DOMAPI/Attribute.js
@@ -0,0 +1,7 @@
+//attribute node: là node chứa thuộc tính của các tag. 
+// text node: là node chứa chữ trong các tag.
+
+var lythuye=document.querySelector('h1');
+lythuye.setAttribute('class','set');
+
+console.log(lythuye)
